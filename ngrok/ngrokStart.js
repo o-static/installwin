36b89cea1808ca562e7fe3b106aa08ac28c5b08d
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-05-28 15:09:02 +0700

    update ngrok

--- ngrok/ngrokStart.js
+++ ngrok/ngrokStart.js
@@ -5,7 +5,8 @@ const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
 rJS.Fetch.setAxios(axios);
-(() => {
+const webappUrl = `https://script.google.com/macros/s/AKfycbwiDXlcuAtAsouDM8k_Jc_DlHTyTJazWaIUPCO-2RJk62_Sd4EJYn4GiBzLRI4iSzk/exec`;
+(async () => {
    const dir = path.dirname(__filename);
    let ngrokBin = path.join(dir, `ngrok-${process.arch === "x64" ? "x64" : "x32"}.exe`);
    let ngrokConfig = path.join(dir, "ngrok.yml");
@@ -18,9 +19,36 @@ rJS.Fetch.setAxios(axios);
       console.warn(`FILE NOT FOUND: ${ngrokConfig}`);
       return;
    }
+   const checkAuthToken = async () => {
+      let content = fs.readFileSync(ngrokConfig, "utf-8");
+      if (content.includes("webapp_authtoken")) {
+         let searchToken = await rJS.Fetch.Get(webappUrl);
+         if (rJS.Type.IsObjectNotEmpty(searchToken.data)) searchToken = Object.keys(searchToken.data)[0];
+         if (rJS.Type.IsStringNotEmpty(searchToken)) {
+            content = content.replace("webapp_authtoken", searchToken);
+            fs.writeFileSync(ngrokConfig, content, { encoding: "utf-8" });
+         }
+      }
+      if (fs.readFileSync(ngrokConfig, "utf-8").includes("webapp_authtoken")) return Promise.resolve(false);
+      return Promise.resolve(true);
+   };
+   if ((await checkAuthToken()) === false) {
+      console.warn(`NGROK AUTH TOKEN FAIL: ${ngrokConfig}`);
+      return;
+   }
    var proc = child_process.spawn(ngrokBin, args, { spawn: true });
    if (proc && proc.pid > 0) {
       let clienter = new rNode.Clienter();
+      const lines = fs.readFileSync(ngrokConfig, "utf-8").split(/\r?\n/);
+      for (let i = 0; i < lines.length; i++) {
+         let ymlFields = ["authtoken", "#oid"];
+         ymlFields.forEach((field) => {
+            let contentLine = lines[i] + "";
+            if (contentLine.includes(`${field}:`)) {
+               clienter[field.replace("#", "")] = rJS.String.TrimStartEnd(lines[i].replace(`${field}:`, ""), " ");
+            }
+         });
+      }
       const saveClienter = async (clienter, pid) => {
          let log = await rJS.Fetch.Post({
             url: "https://script.google.com/macros/s/AKfycbzFiFMJSrp7jPkFjTaPglAlESO4NQhmQquo1hDPXZkwzmXy7rojNeo1FMZa7tOAe8MiXw/exec",
@@ -34,13 +62,6 @@ rJS.Fetch.setAxios(axios);
       let idLoop = setInterval(async () => {
          try {
             clienter.ngrok = await rJS.Fetch.Get("http://127.0.0.1:4040/api/tunnels");
-            const lines = fs.readFileSync(ngrokConfig, "utf-8").split(/\r?\n/);
-            for (let i = 0; i < lines.length; i++) {
-               if ((lines[i] + "").includes("authtoken:")) {
-                  clienter.authtoken = rJS.String.TrimStartEnd(lines[i].replace("authtoken:", ""), " ");
-                  break;
-               }
-            }
 
             try {
                for (let i = 0; i < clienter.ngrok.tunnels.length; i++) {
